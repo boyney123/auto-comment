ecf95591e8faa5bfdcf581fc50f048293a3e219c
James Ford <psyked84@gmail.com>
2018-09-16 17:23:45 +0100

    Expand the supported event triggers to include all available types

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-module.exports = app => {
-  app.on("issues.opened", async context => {
-    const config = await context.config(`auto-comment.yml`);
-    const { issueOpened } = config || {};
+const eventTypes = {
+  issue: [
+    "assigned",
+    "unassigned",
+    "labeled",
+    "unlabeled",
+    "opened",
+    "edited",
+    "milestoned",
+    "demilestoned",
+    "closed",
+    "reopened"
+  ],
+  pull_request: [
+    "assigned",
+    "unassigned",
+    "review_requested",
+    "review_request_removed",
+    "labeled",
+    "unlabeled",
+    "opened",
+    "edited",
+    "closed",
+    "reopened"
+  ]
+};
 
-    const params = context.issue({ body: issueOpened });
-
-    // Post a comment on the issue
-    return context.github.issues.createComment(params);
+function toCamelCase(input) {
+  return input.toLowerCase().replace(/[\.|_](.)/g, function(match, group1) {
+    return group1.toUpperCase();
   });
+}
 
-  app.on("pull_request.opened", async context => {
-    const config = await context.config(`auto-comment.yml`);
-    const { pullRequestOpened } = config || {};
+module.exports = app => {
+  const scopes = Object.keys(eventTypes);
+  scopes.map(scope => {
+    const events = eventTypes[scope];
+    events.map(event => {
+      const eventName = `${scope}.${event}`;
+      app.on(eventName, async context => {
+        const config = await context.config(`auto-comment.yml`);
+        const templateKey = toCamelCase(eventName);
+        const templateData = config[templateKey];
+        if (templateData) {
+          const params = context.issue({ body: templateData });
 
-    const params = context.issue({ body: pullRequestOpened });
+          // Post a comment on the issue
+          return context.github.issues.createComment(params);
+        }
 
-    // Post a comment on the issue
-    return context.github.issues.createComment(params);
+        return false;
+      });
+    });
   });
 };
